9faced69c9f7c50d780cd07c5f429b4fa076fae5
Eugene Kulabuhov <eugene.kulabuhov@gmail.com>
2015-01-16 18:01:49 +0000

    Added dropzone + handling all the image transformations

--- index.js
+++ index.js
@@ -48,4 +48,52 @@ var server = app.listen(3000, function() {
 
 app.use(express.static(__dirname));
 app.use('/photo', imageware);
-app.use(multer({ dest: './uploads'}));
+
+app.post('/api/photo', multer(
+{ 
+    dest: './uploads', 
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path + 'size ' + file.buffer.length);
+	    new ExifImage({ image : file.buffer }, function (error, exifData) {
+	        if (error)
+	            console.log('Error: '+error.message);
+	        else {
+	            console.log(exifData); 
+	            new jimp(file.buffer, function() { 
+		            // EK: refer to EXIF_Orientations.jpg 
+		            // (taken from http://www.daveperrett.com/articles/2012/07/28/exif-orientation-handling-is-a-ghetto/)
+	            	var result = fixOrientation(this, exifData.image.Orientation);
+	            	result.write('./uploads/' + file.originalname);
+	            });
+	        }
+	    });
+    },
+    inMemory: true
+}));
+
+app.post('/api/photo', function (req, res, next) {
+	res.send(req.files.file.originalname);
+	next();
+});
+
+function fixOrientation(jimpImage, orientation) {
+	
+	switch (orientation) {
+		case 1:
+			return jimpImage;
+		case 2:
+			return jimpImage.horizontalFlip();
+	    case 3:
+	        return jimpImage.horizontalFlip().verticalFlip();
+	    case 4:
+	        return jimpImage.verticalFlip();
+	    case 5:
+	        return jimpImage.rotateCW().horizontalFlip();
+	    case 6:
+	        return jimpImage.rotateCW();
+	    case 7:
+	        return jimpImage.horizontalFlip().rotateCW();
+	    case 8:
+	        return jimpImage.rotateCW().horizontalFlip().verticalFlip();
+    }
+}
